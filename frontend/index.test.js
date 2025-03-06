9a9cc91f4f781422ec4160b1f9d716a8b976e804
Tim Sprowl <tim@email>
2025-03-06 14:06:08 -0500

    comment out tests that don't reference current functionality

--- frontend/index.test.js
+++ frontend/index.test.js
@@ -9,13 +9,13 @@ const setup = () => {
   window.SILENT = true
 }
 // test server's port is randomly assigned and then browser-util finds out the port through process.env.PORT
-autotest(command.post, { setup })(filename, "root", { arg: "world" })("hello world")
-autotest(command.get, { setup })(filename, "root", { arg: "world" })("hello world")
+// autotest(command.post, { setup })(filename, "root", { arg: "world" })("hello world")
+// autotest(command.get, { setup })(filename, "root", { arg: "world" })("hello world")
 autotest(command.factory(filename), { setup })("root", { arg: "world" })("hello world")
 
-const setupCookie = () => {
-  document.cookie = `${base}=http://localhost:0; path=/;`
-}
-autotest(command.post, { setup: setupCookie, error: true })(filename, "root", {
-  arg: "world",
-})(expect.objectContaining({ message: expect.stringMatching(/ECONNREFUSED/u) }))
+// const setupCookie = () => {
+//   document.cookie = `${base}=http://localhost:0; path=/;`
+// }
+// autotest(command.post, { setup: setupCookie, error: true })(filename, "root", {
+//   arg: "world",
+// })(expect.objectContaining({ message: expect.stringMatching(/ECONNREFUSED/u) }))
